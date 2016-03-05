02f294e13ae843ed865a5577b914382a7696bb7f
Tyler Neveldine <tneveldine@gmail.com>
2016-03-05 13:40:36 -0500

    Create VideoScreenView class
    - subclass of GPUImageView

---
+++ Unleash Your Demon/VideoScreenView.swift
@@ -0,0 +1,22 @@
+//
+//  VideoScreen.swift
+//  Unleash Your Demon
+//
+//  Created by Tyler Neveldine on 3/5/16.
+//  Copyright © 2016 Tyler Neveldine. All rights reserved.
+//
+
+import UIKit
+import GPUImage
+
+class VideoScreenView: GPUImageView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
