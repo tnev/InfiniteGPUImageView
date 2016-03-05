b115957b51040d045a8419b4b1d73d6fcb8c723c
Tyler Neveldine <tneveldine@gmail.com>
2016-03-05 13:39:54 -0500

    Create VideoCamera class
    - subclass of GPUImageVideoCamera

---
+++ Unleash Your Demon/VideoCamera.swift
@@ -0,0 +1,19 @@
+//
+//  VideoCamera.swift
+//  Unleash Your Demon
+//
+//  Created by Tyler Neveldine on 3/5/16.
+//  Copyright © 2016 Tyler Neveldine. All rights reserved.
+//
+
+import UIKit
+import GPUImage
+
+class VideoCamera: GPUImageVideoCamera {
+    
+    override init() {
+        super.init(sessionPreset: AVCaptureSessionPreset1280x720, cameraPosition: AVCaptureDevicePosition.Back)
+        horizontallyMirrorFrontFacingCamera = true
+    }
+    
+}
